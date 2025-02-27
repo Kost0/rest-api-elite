ed10ba4c1927cbd9d004793fa642b5bedfc35894
Kostya Lavrenov <lavrenov.konstantin17@gmail.com>
2025-02-27 17:57:54 +0300

    JWT secure

--- main.go
+++ main.go
@@ -1,9 +1,87 @@
 package main
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("my_secret_key")
+
+func generateToken(username string) (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+var users = []Credentials{
+	{Username: "user1", Password: "12345"},
+	{Username: "user2", Password: "secret"},
+	{Username: "user3", Password: "8068"},
+}
+
+func login(c *gin.Context) {
+	var creds Credentials
+	if err := c.BindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+		return
+	}
+	found := false
+	for _, user := range users {
+		if creds.Username == user.Username && creds.Password == user.Password {
+			found = true
+			break
+		}
+	}
+	if !found {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+		return
+	}
+
+	token, err := generateToken(creds.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not create token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		claims := &Claims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
 
 type Note struct {
 	ID          string `json:"id"`
@@ -24,11 +102,16 @@ var orders = []Note{
 func main() {
 	router := gin.Default()
 
-	router.GET("/orders", getOrders)
+	router.POST("/login", login)
+
+	protected := router.Group("/")
+	protected.Use(authMiddleware())
+	{
+		protected.GET("/orders", getOrders)
+		protected.POST("/orders", createOrder)
+	}
 
 	router.GET("/orders/:id", getOrderByID)
-
-	router.POST("/orders", createOrder)
 
 	router.PUT("/orders/:id", updateOrder)
 
